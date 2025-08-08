6ee452a39e9b1adbbd4b50f174c000de341733ea
eb3141 <elliott_barthel@outlook.com>
2025-08-08 12:34:10 +0000

    Deploy push v1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,28 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/landscape.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Two adult males in military uniform smiling";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Georgia Tech student earning a B.S. in Economics and International Affairs. I'm passionate about leveraging technology to create innovative solutions for complex problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Excel/VBA",
+  "Data analysis",
+  "Modeling and Simulation",
+  "ArcGis AFSIM Palantir",
+  "Financial Analysis",
+  "Python-learner",
+  "Languages: English (C1), German (C2), Mandarin (A1)",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my experience in defense tech, financial analysis, and data science, I continually look for novel ways to be an intrapreneur.";
 
 const About = () => {
   return (
